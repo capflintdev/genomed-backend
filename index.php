6de77c0f45895d75051c629efb6ecd47d4455b65
futbnj <d.kolisnichenko.sas@gmail.com>
2022-01-19 14:35:00 +0300

    index file changes

--- index.php
+++ index.php
@@ -1,6 +1,25 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+const API_PATH = __DIR__ . '/../api';
+
+use App\Controllers\CategoriesController;
+
+$router = new \App\Router();
+$router
+    ->register('/api/categories', [CategoriesController::class, 'read'], 'get')
+    ->register('/api', [CategoriesController::class, 'errorController'], 'get')
+    ->register('/api/all', [\App\Controllers\TestsController::class, 'index'], 'get');
+try {
+    $router->resolve($_SERVER['REQUEST_URI'], strtolower($_SERVER['REQUEST_METHOD']));
+} catch (\App\Exceptions\RouteNotFoundException $e) {
+    echo $e->getMessage();
+}
+
+echo '<br>';
 echo 'http host ' . $_SERVER['HTTP_HOST'];
 echo '<br>';
 echo 'SCRIPT_FILENAME ' . $_SERVER['SCRIPT_FILENAME'];
@@ -10,4 +29,5 @@ echo '<br>';
 echo 'DOCUMENT_URI' . $_SERVER['DOCUMENT_URI'];
 echo '<br>';
 echo 'HTTP_HOST' . $_SERVER['HTTP_HOST'];
+echo '<br>';
 var_dump($_SERVER);
