b85e929ff1fd096745f2cdf7e49081fdcd57297c
futbnj <d.kolisnichenko.sas@gmail.com>
2022-01-19 14:09:14 +0300

    index file changes

--- index.php
+++ index.php
@@ -1,4 +1,26 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Credentials: true");
+header("Content-Type: application/json");
+
 require_once __DIR__ . '/vendor/autoload.php';
 
-echo $_SERVER['HTTP_HOST'];
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+const API_PATH = __DIR__ . '/../api';
+
+use App\Controllers\CategoriesController;
+
+$router = new \App\Router();
+$router
+    ->register('/api/categories', [CategoriesController::class, 'read'], 'get')
+    ->register('/api', [CategoriesController::class, 'errorController'], 'get')
+    ->register('/api/all', [\App\Controllers\TestsController::class, 'index'], 'get');
+try {
+    $router->resolve($_SERVER['REQUEST_URI'], strtolower($_SERVER['REQUEST_METHOD']));
+} catch (\App\Exceptions\RouteNotFoundException $e) {
+    echo $e->getMessage();
+}
