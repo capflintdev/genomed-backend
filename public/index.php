d734481b0e0213ba1f2adfd0d01695a4f091f0f4
futbnj <d.kolisnichenko.sas@gmail.com>
2022-01-19 14:01:07 +0300

    headers added

--- public/index.php
+++ public/index.php
@@ -1,4 +1,9 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Credentials: true");
+header("Content-Type: application/json");
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
