aa287af7b174c886d3935f5cfc401ed6140ee413
futbnj <d.kolisnichenko.sas@gmail.com>
2022-01-19 15:05:50 +0300

    index file changes

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
-$dotenv->load();
+//$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+//$dotenv->load();
 
-const API_PATH = __DIR__ . '/../api';
+//const API_PATH = __DIR__ . '/../api';
 
 use App\Controllers\CategoriesController;
 
