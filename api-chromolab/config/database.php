997bebb743c86de600e89285383690e2ad87ead0
futbnj <d.kolisnichenko.sas@gmail.com>
2022-01-19 16:01:50 +0300

    database conn

--- api-chromolab/config/database.php
+++ api-chromolab/config/database.php
@@ -3,10 +3,10 @@
 class Database {
 
     // укажите свои учетные данные базы данных
-    private $host = "77.222.56.111";
-    private $db_name = "testchebur_chrom";
-    private $username = "testchebur_chrom";
-    private $password = "!Ery4k5*JVQR_u";
+    private $host = "${db.HOSTNAME}";
+    private $db_name = "genomed";
+    private $username = "${db.USERNAME}";
+    private $password = "${db.PASSWORD}";
     public $conn;
 
     // получаем соединение с БД
